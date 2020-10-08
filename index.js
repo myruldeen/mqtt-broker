dbca7eef58afa1016db238f0bacf4fcfa866e3ac
myruldeen <funstudy10@gmail.com>
2020-10-08 16:39:45 +0800

    change to aedes

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 let SSL_KEY = __dirname + '/certs/key.pem';
 let SSL_CERT = __dirname + '/certs/certificate.pem';
-let MONGOURL = 'mongodb://localhost:27017/mqtt';
-// let MONGOURL = 'mongodb://mirul123:mirul123@cluster0-shard-00-00-uexgi.mongodb.net:27017,cluster0-shard-00-01-uexgi.mongodb.net:27017,cluster0-shard-00-02-uexgi.mongodb.net:27017/iotfwjs?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+let MONGOURL = 'mongodb://localhost:27017/aedes';
 
 module.exports = {
-    id: 'broker',
-    stats: false,
-    port: 8443,
-    logger: {
-        name: 'iotfwjs',
-        level: 'debug'
-    },
-    secure: {
-        keyPath: SSL_KEY,
-        certPath: SSL_CERT,
-    },
-    backend: {
-        type: 'mongodb',
-        url: MONGOURL
-    },
+    protos: ['tcp'],
+    host: '127.0.0.1',
+    port: 1883,
+    wsPort: 3000,
+    wssPort: 4000,
+    tlsPort: 8883,
+    brokerId: 'aedes-cli',
+    credentials: null,
     persistence: {
-        factory: 'mongo',
+      name: 'mongodb',
+      options: {
         url: MONGOURL
-    }
-};
+      }
+    },
+    mq: {
+      name: 'mongodb',
+      options: {
+        url: MONGOURL
+      }
+    },
+    key: SSL_KEY,
+    cert: SSL_CERT,
+    rejectUnauthorized: true,
+    verbose: false,
+    veryVerbose: false,
+    noPretty: false
+  }
 
 
